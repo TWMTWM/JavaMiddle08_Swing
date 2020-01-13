9fa7e1d0801123e81d37793dea41c66b2d5227ec
TWM <15152806231@163.com>
2020-01-13 20:47:37 +0800

    文本框

---
+++ src/cn/gtmap/component/Test09_JTextField.java
@@ -0,0 +1,48 @@
+package cn.gtmap.component;
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+/**
+ * 文本框
+ *	JTextField 输入框
+ *	setText 设置文本
+ *	getText 获取文本
+ *	JTextField 是单行文本框，如果要输入多行数据，请使用JTextArea
+ *	tfPassword.grabFocus(); 表示让密码输入框获取焦点
+ */
+public class Test09_JTextField {
+	public static void main(String[] args) {
+		JFrame f = new JFrame("LoL");
+		f.setSize(400, 300);
+		f.setLocation(200, 200);
+		
+		f.setLayout(new FlowLayout());
+		JLabel lName = new JLabel("账号：");
+		//输入框
+		JTextField tfName = new JTextField("");
+		tfName.setText("请输入账号");
+		tfName.setPreferredSize(new Dimension(80, 30));
+		
+		JLabel lPassword = new JLabel("密码：");
+		//输入框
+		JTextField tfPassword = new JTextField("");
+		tfPassword.setText("请输入密码");
+		tfPassword.setPreferredSize(new Dimension(80,30));
+		
+		f.add(lName);
+		f.add(tfName);
+		f.add(lPassword);
+		f.add(tfPassword);
+		
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		f.setVisible(true);
+		tfPassword.grabFocus();
+		
+	}
+}
