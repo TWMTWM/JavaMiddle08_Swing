41184be7578d8aca824352bd8d6430a7d258e97c
TWM <15152806231@163.com>
2020-01-13 19:11:09 +0800

    CardLayout

---
+++ src/cn/gtmap/layouter/Test06_GardLayout.java
@@ -0,0 +1,14 @@
+package cn.gtmap.layouter;
+
+/**
+ * CardLayout
+ *
+ */
+public class Test06_GardLayout {
+	public static void main(String[] args) {
+		/**
+		 * 因为CardLayout需要用到面板，
+		 * 	JComboBox这些内容暂时还没学的内容，所以放在后面讲： CardLayout
+		 */
+	}
+}
